08f7e4071f11ce1ff4bd926c67f6cef11a9203f0
TheMarlboroMan <marlborometal@gmail.com>
2018-04-13 21:51:58 +0200

    Removed input demo from here. It belongs in other project

--- main.cpp
+++ main.cpp
@@ -11,27 +11,6 @@ tools::log	applog("logs/app.log");
 
 int main(int argc, char ** argv) {
 
-	std::cout<<"Hello >>"<<std::endl;
-
-	//TODO: SHOULD CLEAR ITSELF AFTER EACH PRESS!!.
-
-	tools::terminal_in	ti;
-	while(true) {
-		std::flush(std::cout);
-		auto id=ti.get();
-		if(id) {
-			if(id.is_char()) {
-				std::cout<<"You said "<<id.c<<" ["<<(int)id.c<<"]"<<std::endl;
-				if(id.c=='x') break;
-			}
-			else if(id.is_arrow()) {
-				std::cout<<"Good arrow press..."<<std::endl;
-			}
-		}
-	}
-
-	return 0;
-
 	try {
 		app::driver d;
 		d.run();
